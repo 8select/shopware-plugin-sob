98de5fca75a98b4c69b71735f9dbb9c86a037cc5
Andreas Kleiber <andreas.kleiber@8select.de>
2017-07-06 10:35:44 +0200

    catch CSE errors

--- EightSelect/Resources/views/frontend/_public/src/js/jquery.8select-csePlugin.js
+++ EightSelect/Resources/views/frontend/_public/src/js/jquery.8select-csePlugin.js
@@ -16,16 +16,24 @@
             $.subscribe('plugin/swEmotionLoader/onLoadEmotionFinished', $.proxy(me.onLoadEmotionFinished, me));
         },
         onRequestData: function() {
-            var me = this;
+            var me = this; 
             var newSku = $(me.defaults.skuSelector).text();
             if (me.currentSku !== newSku && typeof _8select !== "undefined" && _8select.reinitSys) {
-                _8select.reinitSys(newSku, me.currentSku);
+                try {
+                    _8select.reinitSys(newSku, me.currentSku);
+                } catch (error) {
+                    console.error(error)
+                }
             }
             me.currentSku = newSku;
         },
         onLoadEmotionFinished: function() {
             if (typeof _8select !== "undefined" && _8select.initCSE) {
-                _8select.initCSE();
+                try {
+                    _8select.initCSE();
+                } catch (error) {
+                    console.error(error)
+                }
             }
         },
         destroy: function() {
