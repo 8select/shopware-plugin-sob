f8e87713527a72dfa2e5a6a95fff7583118fe086
Elizabeth Dunphy <edunphy@1drop.de>
2018-05-02 16:07:50 +0200

    [TASK][ESSP-23] adapt shell script

--- bin/release.sh
+++ bin/release.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash -e
 
-VERSION=${1}
+VERSION=2
 
-cp EightSelect/plugin.xml EightSelect/plugin.org.xml
-sed -i '' "s@__VERSION__@${VERSION}@g" EightSelect/plugin.xml
-zip -q -r dist/8select_CSE_Shopware-5.2_${VERSION}.zip EightSelect
-mv EightSelect/plugin.org.xml EightSelect/plugin.xml
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cp -r $SCRIPTPATH/../EightSelect $SCRIPTPATH/EightSelect
+sed -i '' "s@__VERSION__@${VERSION}@g" $SCRIPTPATH/EightSelect/plugin.xml
+cd $SCRIPTPATH/EightSelect && composer install
+zip -r $SCRIPTPATH/../dist/8select_CSE_Shopware-5.2_${VERSION}.zip $SCRIPTPATH/EightSelect
+rm -rf $SCRIPTPATH/EightSelect
 echo "created release ${VERSION} at dist/8select_CSE_Shopware-5.2_${VERSION}.zip"
