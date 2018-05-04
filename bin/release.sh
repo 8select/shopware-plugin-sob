db54a4f622f0f5c6fae6af1eb4a650b4fd20393e
Elizabeth Dunphy <edunphy@1drop.de>
2018-05-04 11:31:54 +0200

    [TASK][ESSP-23] change build script so it doesn't use absolut paths

--- bin/release.sh
+++ bin/release.sh
@@ -2,10 +2,12 @@
 
 VERSION=2
 
-cp -r EightSelect bin/EightSelect
-sed -i '' "s@__VERSION__@${VERSION}@g" bin/EightSelect/plugin.xml
-cd bin/EightSelect && composer install && cd ../..
-zip -r dist/8select_CSE_Shopware-5.2_${VERSION}.zip bin/EightSelect
-rm -rf bin/EightSelect
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+cp -r $SCRIPTPATH/../EightSelect $SCRIPTPATH/EightSelect
+sed -i '' "s@__VERSION__@${VERSION}@g" $SCRIPTPATH/EightSelect/plugin.xml
+cd $SCRIPTPATH/EightSelect && composer install && cd ..
+zip -r ../dist/8select_CSE_Shopware-5.2_${VERSION}.zip EightSelect
+rm -rf $SCRIPTPATH/EightSelect
 echo "created release ${VERSION} at dist/8select_CSE_Shopware-5.2_${VERSION}.zip"
 
