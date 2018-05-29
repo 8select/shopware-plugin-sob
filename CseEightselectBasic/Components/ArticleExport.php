4c1c4fa416f16ca74ecc7c09c9c380df70317f76
Oliver Eglseder <oeglseder@1drop.de>
2018-05-29 11:55:09 +0200

    Use correct plugin config provider

--- CseEightselectBasic/Components/ArticleExport.php
+++ CseEightselectBasic/Components/ArticleExport.php
@@ -113,8 +113,9 @@ class ArticleExport
         }
 
         $start = time();
-        $config = Shopware()->Config();
-        $feedId = $config->get('8s_feed_id');
+        $config = Shopware()->Container()->get('shopware.plugin.cached_config_reader');
+        $feedId = $config->getByPluginName('CseEightselectBasic')['8s_feed_id'];
+
         $feedType = 'product_feed';
         $timestampInMillis = round(microtime(true) * 1000);
         $filename = sprintf('%s_%s_%d.csv', $feedId, $feedType, $timestampInMillis);
