791a77d2a029aff1437453e93bdeac970351b86e
Norman Steger <norman.steger@8select.de>
2018-06-11 14:09:41 +0200

    add plugin validation to attribute mapping dialog

--- CseEightselectBasic/Resources/views/backend/cse_eightselect_basic_attribute_config/view/list/eightselect_attribute.js
+++ CseEightselectBasic/Resources/views/backend/cse_eightselect_basic_attribute_config/view/list/eightselect_attribute.js
@@ -47,6 +47,77 @@ Ext.define(
           }
         }
       };
+    },
+
+    initComponent: function () {
+      var me = this
+
+      var stateCheck = Ext.Ajax.request({ async: false, url: '{url controller=CseEightselectBasicManualExport action=checkForActiveState}' })
+      var apiCheck = Ext.Ajax.request({ async: false, url: '{url controller=CseEightselectBasicManualExport action=checkForApiId}' })
+      var feedCheck = Ext.Ajax.request({ async: false, url: '{url controller=CseEightselectBasicManualExport action=checkForFeedId}' })
+        
+      var active = Ext.decode(stateCheck.responseText).active
+      var apiId = Ext.decode(apiCheck.responseText).apiId
+      var feedId = Ext.decode(feedCheck.responseText).feedId
+
+      var EMPTY_API_ID_MESSAGE = "Keine API ID hinterlegt. Bitte überprüfen Sie Ihre Plugin-Einstellungen oder wenden Sie sich an 8select."
+      var INVALID_API_ID_MESSAGE = "API ID ungültig. Bitte überprüfen Sie Ihre Plugin-Einstellungen oder wenden Sie sich an 8select."
+
+      var EMPTY_FEED_ID_MESSAGE = "Keine Feed ID hinterlegt. Bitte überprüfen Sie Ihre Plugin-Einstellungen oder wenden Sie sich an 8select."
+      var INVALID_FEED_ID_MESSAGE = "Feed ID ungültig. Bitte überprüfen Sie Ihre Plugin-Einstellungen oder wenden Sie sich an 8select."
+
+      var PLUGIN_NOT_ACTIVE = "Plugin ist nicht aktiv. Bitte überprüfen Sie Ihre Plugin-Einstellungen oder wenden Sie sich an 8select."
+
+      function enableAttributeMapping() {
+        me.setDisabled(false)
+      }
+
+      function disableAttributeMapping() {
+        me.setDisabled(true)
+      }
+
+      function checkForActiveState(validateState) {
+        if (!validateState || validateState === null) {
+          Shopware.Notification.createGrowlMessage("", PLUGIN_NOT_ACTIVE)
+        }
+      }
+
+      function checkForApiId(id) {
+        if (!id || id === null ||id.length === 0) {
+          Shopware.Notification.createGrowlMessage("", EMPTY_API_ID_MESSAGE)
+        }
+
+        if (id && id !== null && id.length !== 36) {
+          Shopware.Notification.createGrowlMessage("", INVALID_API_ID_MESSAGE)
+        }
+      }
+
+      function checkForFeedId(id) {
+        if (!id || id === null ||id.length === 0) {
+          Shopware.Notification.createGrowlMessage("", EMPTY_FEED_ID_MESSAGE)
+        }
+
+        if (id && id !== null && id.length !== 36) {
+          Shopware.Notification.createGrowlMessage("", INVALID_FEED_ID_MESSAGE)
+        }
+      }
+
+      function validatePluginConfig(callback) {
+
+          checkForActiveState(active)
+          checkForApiId(apiId)
+          checkForFeedId(feedId)
+
+          if (active !== null && apiId !== null && feedId !== null) {
+            if (active && apiId.length === 36 && feedId.length === 36) {
+              callback()
+            }
+          }
+      }
+
+      me.callParent(arguments);
+      disableAttributeMapping()
+      validatePluginConfig(enableAttributeMapping)
     }
   }
 );
