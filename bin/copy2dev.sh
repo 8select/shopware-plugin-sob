752f21478121b1c64f8b81a6229e8e582935bfe7
Norman Steger <norman.steger@8select.de>
2018-06-13 08:33:42 +0200

    rewrite endpoints for staging on copy2dev

--- bin/copy2dev.sh
+++ bin/copy2dev.sh
@@ -10,6 +10,11 @@ PLUGIN_NAME="CseEightselectBasic"
 PLUGIN_DIR="${DESTINATION}/${PLUGIN_NAME}"
 
 cp -r "${CURRENT_DIR}/../${PLUGIN_NAME}" "${DESTINATION}"
+
 sed -i '' "s@__VERSION__@${VERSION}@g" ${PLUGIN_DIR}/plugin.xml
+sed -i '' "s@__BUCKET__@productfeed-prod.staging@g" ${PLUGIN_DIR}/Components/AWSUploader.php
+sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${PLUGIN_DIR}/Resources/views/frontend/index/header.tpl
+sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${PLUGIN_DIR}/Resources/views/frontend/checkout/finish.tpl
+
 
 echo "Working directory successfully copied to ${DESTINATION}"
