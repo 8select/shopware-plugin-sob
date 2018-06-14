5639088e06af97b3cbed3ef9f6749ff01f95ceff
Norman Steger <norman.steger@8select.de>
2018-06-14 11:29:02 +0200

    fix update plugin dir in docker container using temp dir

--- bin/copy2dev.sh
+++ bin/copy2dev.sh
@@ -5,21 +5,22 @@ SOURCE="${BASH_SOURCE[0]}"
 CURRENT_DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 CONTAINER=${1}
 VERSION=${2}
-DOCKERDIR="/shopware/custom/plugins"
+PLUGINDIR="/shopware/custom/plugins"
+SHOPWARE_CLI_BIN="bin/console"
 
 PLUGIN_NAME="CseEightselectBasic"
-PLUGIN_DIR="${CONTAINER}:${DOCKERDIR}/${PLUGIN_NAME}"
+PLUGIN_DIR="${CONTAINER}:${PLUGINDIR}/${PLUGIN_NAME}"
+TEMP_DIR=`mktemp -d`
 
-sed -i '' "s@__VERSION__@${VERSION}@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/plugin.xml
-sed -i '' "s@__BUCKET__@productfeed-prod.staging@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Components/AWSUploader.php
-sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Resources/views/frontend/index/header.tpl
-sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Resources/views/frontend/checkout/finish.tpl
+cp -r ${CURRENT_DIR}/../${PLUGIN_NAME} ${TEMP_DIR}
 
-docker cp -a "${CURRENT_DIR}/../${PLUGIN_NAME}" "${CONTAINER}:${DOCKERDIR}"
+sed -i '' "s@__VERSION__@${VERSION}@g" ${TEMP_DIR}/${PLUGIN_NAME}/plugin.xml
+sed -i '' "s@__BUCKET__@productfeed-prod.staging@g" ${TEMP_DIR}/${PLUGIN_NAME}/Components/AWSUploader.php
+sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${TEMP_DIR}/${PLUGIN_NAME}/Resources/views/frontend/index/header.tpl
+sed -i '' "s@__BUCKET__@wgt-prod.staging@g" ${TEMP_DIR}/${PLUGIN_NAME}/Resources/views/frontend/checkout/finish.tpl
 
-sed -i '' "s@${VERSION}@__VERSION__@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/plugin.xml
-sed -i '' "s@productfeed-prod.staging@__BUCKET__@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Components/AWSUploader.php
-sed -i '' "s@wgt-prod.staging@__BUCKET__@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Resources/views/frontend/index/header.tpl
-sed -i '' "s@wgt-prod.staging@__BUCKET__@g" ${CURRENT_DIR}/../${PLUGIN_NAME}/Resources/views/frontend/checkout/finish.tpl
+docker cp "${TEMP_DIR}/${PLUGIN_NAME}" "${CONTAINER}:${PLUGINDIR}"
+docker exec -i ${CONTAINER} sh -c "php ${SHOPWARE_CLI_BIN} sw:plugin:refresh"
+docker exec -i ${CONTAINER} sh -c "php ${SHOPWARE_CLI_BIN} sw:plugin:update CseEightselectBasic"
 
-echo "Working directory successfully copied to ${DOCKERDIR} in docker container ${CONTAINER}"
+echo "Working directory successfully copied to ${PLUGINDIR} in docker container ${CONTAINER}"
