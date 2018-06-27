f6536a6dc881aa798f543fd1088b74bfc22d1903
Norman Steger <norman.steger@8select.de>
2018-06-27 15:15:03 +0200

    simplify sql queries in FeedLogger.php

--- CseEightselectBasic/Components/FeedLogger.php
+++ CseEightselectBasic/Components/FeedLogger.php
@@ -15,7 +15,6 @@ class FeedLogger {
     $sql = 'INSERT INTO `' . self::TABLE_NAME . '` (feed_name, last_run)
             VALUES ("' . $feedName . '", NOW())
             ON DUPLICATE KEY UPDATE
-              feed_name = "'. $feedName . '",
               last_run = NOW();';
 
     if (getenv('ES_DEBUG')) {
@@ -27,26 +26,18 @@ class FeedLogger {
   }
 
   public static function getLastFeedUpdate($feedName) {
-    $sql = 'SELECT last_run FROM `' . self::TABLE_NAME . '` WHERE feed_name = "' . $feedName . '"';
-    $lastRun = Shopware()->Db()->query($sql)->fetchAll();
-
-    return $lastRun;
+    return Shopware()->Db()->fetchOne('SELECT last_run FROM ' . self::TABLE_NAME . ' WHERE feed_name = "' . $feedName . '";');
   }
 
   public static function createTable() 
   {
-    $sqls = [
-      'DROP TABLE IF EXISTS `' . self::TABLE_NAME . '`;',
-      'CREATE TABLE `' . self::TABLE_NAME . '` (
-        `feed_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-        `last_run` datetime DEFAULT NULL,
-        PRIMARY KEY (`feed_name`)
-        ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;'
-    ];
+    $sql = 'CREATE TABLE IF NOT EXISTS`' . self::TABLE_NAME . '` (
+              `feed_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+              `last_run` datetime DEFAULT NULL,
+              PRIMARY KEY (`feed_name`)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;';
 
-    foreach ($sqls as $sql) {
-      Shopware()->Db()->query($sql);
-    }
+    Shopware()->Db()->query($sql);
   }
 
   public static function deleteTable() {
