e968761e3e311511e62fd6d61afe0d85eaad1d3a
Norman Steger <norman.steger@8select.de>
2018-07-16 14:28:21 +0200

    restructure `eightselect_attribute.js` afeter rebase

--- CseEightselectBasic/Resources/views/backend/cse_eightselect_basic_attribute_config/view/list/eightselect_attribute.js
+++ CseEightselectBasic/Resources/views/backend/cse_eightselect_basic_attribute_config/view/list/eightselect_attribute.js
@@ -1,134 +1,134 @@
-Ext.define("Shopware.apps.CseEightselectBasicAttributeConfig.view.list.EightselectAttribute",{
-    extend: "Shopware.grid.Panel",
-    alias: "widget.8select-attributes-grid",
-    id: "8select-attributes-grid",
-    region: "center",
-    listeners: {
-      beforerender: function() {
-        var grid = Ext.getCmp("8select-attributes-grid")
-
-        Ext.create("Shopware.apps.CseEightselectBasicAttributeConfig.store.ShopwareAttribute", {
-          storeId: 'shopware-attribute-label-store'
-        }).load({
-          callback: function(records, operation, success) {
-            if(success) {
-              grid.getView().refresh();
-            }
-          }
-        })
-      }
-    },
-    configure: function() {
-      return {
-        actionColumn: false,
-        addButton: false,
-        deleteButton: false,
-        rowEditing: true,
-        pagingbar: true,
-        columns: {
-          eightselectAttributeLabel: {
-            header: "8select Attribute",
-            width: 250,
-            editor: {
-              editable: false
-            }
-          },
-          eightselectAttributeLabelDescr: {
-            header: "Description",
-            width: 450,
-            renderer: function(value, meta){
-              meta.style = 'white-space: normal;'; 
-              meta.tdAttr = 'style="white-space: normal;"';
-              return value;      
-            },
-            editor: {
-              editable: false
-            }
-          },
-          shopwareAttribute: {
-            header: "Shopware Attribute",
-            renderer: function(val, meta, rec, rowIdx, colIdx, store, view) {
-              var labelStore = Ext.getStore('shopware-attribute-label-store')
-              var currentField = store.data.items[rowIdx].data.shopwareAttribute
-
-              if (!labelStore.data || labelStore.data.items.length === 0) {
-                return "Lade Daten..."
-              }
-              
-              var targetItem = labelStore.data.items.find(function(item) {
-                  return item.data.column_name === currentField
-              })
-
-              if (!targetItem) {
-                return val
-              }
-
-              return targetItem.data.label
-            },
-            editor: {
-              xtype: "combobox",
-              allowBlank: true,
-              multiSelect: true,
-              valueField: "column_name",
-              displayField: "label",
-              tpl: Ext.create('Ext.XTemplate',
-                '<tpl for=".">',
-                  '<div style="padding:5px" class="x-boundlist-item">{literal}{label}{/literal} <i style="color:#ccc">({literal}{column_name}{/literal})</i></div>',
-                '</tpl>'
-              ),
-              listeners: {
-                beforeselect: function() {
-                  if (this.value.length === 1) {
-                      var isArticleField = this.value.some(function(field) {
-                        return field.match(/s\_articles|additionaltext|weight|width|height|length|ean/)
-                      })
-                  }
-                  if (nonPropertyFields.length > 1) {
-                    alert("Es können nur Konfigurator-Gruppen und Eigenschaften mehrfach ausgewählt werden!");
-                    this.clearValue()
-                  }
-
-                  return true;
-                }
-              }
-            },
-            store: Ext.create("Shopware.apps.CseEightselectBasicAttributeConfig.store.ShopwareAttribute"),
+Ext.define("Shopware.apps.CseEightselectBasicAttributeConfig.view.list.EightselectAttribute", {
+  extend: "Shopware.grid.Panel",
+  alias: "widget.8select-attributes-grid",
+  id: "8select-attributes-grid",
+  region: "center",
+  configure: function() {
+    return {
+      actionColumn: false,
+      addButton: false,
+      deleteButton: false,
+      rowEditing: true,
+      pagingbar: true,
+      columns: {
+        eightselectAttributeLabel: {
+          header: "8select Attribute",
+          width: 250,
+          editor: {
             editable: false
+          }
+        },
+        eightselectAttributeLabelDescr: {
+          header: "Description",
+          width: 450,
+          renderer: function(value, meta) {
+            meta.style = "white-space: normal;";
+            meta.tdAttr = 'style="white-space: normal;"';
+            return value;
+          },
+          editor: {
+            editable: false
+          }
+        },
+        shopwareAttribute: {
+          header: "Shopware Attribute",
+          renderer: function(val, meta, rec, rowIdx, colIdx, store, view) {
+            var labelStore = Ext.getStore("shopware-attribute-label-store");
+            var currentField = store.data.items[rowIdx].data.shopwareAttribute;
+
+            if (!labelStore.data || labelStore.data.items.length === 0) {
+              return "Lade Daten...";
+            }
+
+            var targetItem = labelStore.data.items.find(function(item) {
+              return item.data.column_name === currentField;
+            });
+
+            if (!targetItem) {
+              return val;
+            }
+
+            return targetItem.data.label;
+          },
+          editor: {
+            xtype: "combobox",
+            allowBlank: true,
+            multiSelect: true,
+            editable: false,
+            valueField: "column_name",
+            displayField: "label",
+            store: Ext.create("Shopware.apps.CseEightselectBasicAttributeConfig.store.ShopwareAttribute"),
+            tpl: Ext.create(
+              "Ext.XTemplate",
+              '<tpl for=".">',
+              '<div style="padding:5px" class="x-boundlist-item">{literal}{label}{/literal} <i style="color:#ccc">({literal}{column_name}{/literal})</i></div>',
+              "</tpl>"
+            ),
+            listeners: {
+              change: function() {
+                var nonPropertyFields = this.value.filter(function(field) {
+                  return field.match(/s\_articles|additionaltext|weight|width|height|length|ean/);
+                });
+
+                if (nonPropertyFields.length > 1) {
+                  alert("Es können nur Konfigurator-Gruppen und Eigenschaften mehrfach ausgewählt werden!");
+                  this.clearValue();
+                }
+
+                return true;
+              }
+            }
           }
         }
       }
-    },
-    initComponent: function() {
-      var me = this;
-  
-      var configValidationRequest = Ext.Ajax.request({
-        async: false,
-        url: "{url controller=CseEightselectBasicConfigValidation action=validate}"
-      });
-  
-      var configValidationResult = Ext.decode(configValidationRequest.responseText).validationResult;
-  
-      var isConfigValid = function(configValidationResult) {
-        return configValidationResult.isValid;
-      };
-  
-      var getErrorMessages = function(configValidationResult) {
-        return configValidationResult.messages;
-      };
-  
-      function enableAttributeMapping() {
-        me.setDisabled(false);
-      }
-  
-      function disableAttributeMapping() {
-        me.setDisabled(true);
-      }
-  
-      me.callParent(arguments);
-      disableAttributeMapping();
-  
-      if (isConfigValid(configValidationResult)) {
-        enableAttributeMapping();
-      }
+    };
+  },
+  initComponent: function() {
+    var me = this;
+
+    var configValidationRequest = Ext.Ajax.request({
+      async: false,
+      url: "{url controller=CseEightselectBasicConfigValidation action=validate}"
+    });
+
+    var configValidationResult = Ext.decode(configValidationRequest.responseText).validationResult;
+
+    var isConfigValid = function(configValidationResult) {
+      return configValidationResult.isValid;
+    };
+
+    var getErrorMessages = function(configValidationResult) {
+      return configValidationResult.messages;
+    };
+
+    function enableAttributeMapping() {
+      me.setDisabled(false);
     }
+
+    function disableAttributeMapping() {
+      me.setDisabled(true);
+    }
+
+    me.callParent(arguments);
+    disableAttributeMapping();
+
+    if (isConfigValid(configValidationResult)) {
+      enableAttributeMapping();
+    }
+  },
+  listeners: {
+    beforerender: function() {
+      var grid = Ext.getCmp("8select-attributes-grid");
+
+      Ext.create("Shopware.apps.CseEightselectBasicAttributeConfig.store.ShopwareAttribute", {
+        storeId: "shopware-attribute-label-store"
+      }).load({
+        callback: function(records, operation, success) {
+          if (success) {
+            grid.getView().refresh();
+          }
+        }
+      });
+    }
+  }
 });
