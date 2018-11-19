b0b7506424732d4060adafa5d699805f020ad736
Norman Steger <norman.steger@8select.de>
2018-11-19 15:45:20 +0100

    respect sorting of variant mappings in aricle image mapper

--- CseEightselectBasic/Components/ArticleImageMapper.php
+++ CseEightselectBasic/Components/ArticleImageMapper.php
@@ -15,7 +15,7 @@ class ArticleImageMapper
            LEFT JOIN s_article_img_mapping_rules aimr on aimr.mapping_id = aim.id
            LEFT JOIN s_article_configurator_option_relations acor ON acor.option_id = aimr.option_id AND acor.article_id = ?
            WHERE ai.articleID = ? AND (acor.id IS NOT NULL OR aimr.id IS NULL)
-           ORDER BY ai.position;";
+           ORDER BY acor.id DESC, ai.position ASC;";
 
     return Shopware()->Db()->query($sql, [$detailId, $articleId])->fetchAll();
   }
