056c055627bc9a1fdb9d540d8f856f07453a6fcd
Dominik Stötter <dominik.stoetter@8select.de>
2018-12-14 16:03:44 +0100

    register export service and use validator service for validation

--- CseEightselectBasic/Services/Export/Export.php
+++ CseEightselectBasic/Services/Export/Export.php
@@ -1,32 +1,33 @@
 <?php
-namespace CseEightselectBasic\Components;
+namespace CseEightselectBasic\Services\Export;
 
 use Shopware;
-class ExportHelper {
-  private static function isTidAndFidInRequest ($request) {
+
+class Export {
+
+  public function __construct() {
+    $container = Shopware()->Container();
+    $this->configValidator = $container->get('cse_eightselect_basic.config.validator');
+    $this->pluginConfig = $container->get('cse_eightselect_basic.dependencies.provider')->getPluginConfig();
+    
+  }
+
+  private function isTidAndFidInRequest ($request) {
     $tenantId = $request->getHeader('8select-com-tid');
     $feedId = $request->getHeader('8select-com-fid');
     
     return $tenantId != null && $feedId != null;
   }
 
-  private static function isTidAndFidConfigured ($config) {
-    $actualTenantId = $config['8s_merchant_id'];
-    $actualFeedId = $config['8s_feed_id'];
-  
-    return $actualTenantId != null || $actualFeedId != null;
+  private function isTidAndFidValid ($request) {
+    return $request->getHeader('8select-com-tid') != $this->pluginConfig['8s_merchant_id'] || $request->getHeader('8select-com-fid') != $this->pluginConfig['8s_merchant_id'];
   }
 
-  private static function isTidAndFidValid ($request, $config) {
-    return $request->getHeader('8select-com-tid') != $config['8s_merchant_id'] || $request->getHeader('8select-com-fid') != $config['8s_merchant_id'];
-  }
-
-  public static function getOffsetAndLimit ($request) {
+  public function getOffsetAndLimit ($request) {
     //@todo maybe use symfony
     $MAX_LIMIT = 100;
     $limit = $request->getParam('limit', 50);
     $offset = $request->getParam('offset', 0);
-  
 
     if(!is_numeric($limit) || !is_numeric($offset)) {
       return false;
@@ -39,27 +40,25 @@ class ExportHelper {
       return false;
     }
 
-
     return ['limit' => $limit, 'offset' => $offset];
   }
 
-  public static function validateExportRequest($response, $request) {
+  public function validateExportRequest($response, $request) {
     $isTidAndFidInRequest = self::isTidAndFidInRequest($request);
     if (!$isTidAndFidInRequest) {
         $response->setHttpResponseCode(404);
         return false;
     }
     
-    $config = Shopware()->Container()->get('cse_eightselect_basic.dependencies.provider')->getPluginConfig();
-    $isTidAndFidConfigured = self::isTidAndFidConfigured($config);
-    if (!$isTidAndFidConfigured) {
-        $response->setHeader('Content-Type', 'text/html');
-        $response->setBody('plugin is not configured');
+    $validationResult = $this->configValidator->validateExportConfig();
+    if (!$validationResult['isValid']) {
+        $response->setHeader('Content-Type', 'application/json');
+        $response->setBody(json_encode($validationResult['violations']));
         $response->setHttpResponseCode(500);
         return false;
     }
     
-    $areTidAndFidValid = self::isTidAndFidValid($request, $config);
+    $areTidAndFidValid = self::isTidAndFidValid($request);
     if (!$areTidAndFidValid) {
         $response->setHeader('Content-Type', 'text/html');
         $response->setBody('wrong tid or fid');
@@ -78,7 +77,7 @@ class ExportHelper {
     return true;
   } 
 
-  public static function getArticlesByOffsetAndLimit($offsetAndLimit) {
+  public function getArticlesByOffsetAndLimit($offsetAndLimit) {
     $offset = $offsetAndLimit['offset'];
     $limit = $offsetAndLimit['limit'];
 
