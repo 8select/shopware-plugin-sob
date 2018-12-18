9f55c71953f100bc416fb95c56d285183a75e33e
Dominik Stötter <dominik.stoetter@8select.de>
2018-12-18 15:44:21 +0100

    split logic into separate services

---
+++ CseEightselectBasic/Services/Response/ErrorBody.php
@@ -0,0 +1,16 @@
+<?php
+namespace CseEightselectBasic\Services\Response;
+
+class ErrorBody {
+  public function getInternalServerErrorBody($message) {
+    return json_encode($this->getError('Internal Server Error', $message));
+  }
+
+  public function getBadRequestBody($message) {
+    return json_encode($this->getError('Bad Request', $message));
+  }
+
+  public function getError($error, $message) {
+    return ["error" => $error, "message" => $message];
+  }
+}
