18dc2cc4db4e105d9b52ee459f3c99641d6767dc
Dominik Stötter <dominik.stoetter@8select.de>
2018-12-21 08:41:09 +0100

    refactor after rebase

--- CseEightselectBasic/Services/Response/ErrorBody.php
+++ CseEightselectBasic/Services/Response/ErrorBody.php
@@ -1,16 +1,21 @@
 <?php
+
 namespace CseEightselectBasic\Services\Response;
 
-class ErrorBody {
-  public function getInternalServerErrorBody($message) {
-    return json_encode($this->getError('Internal Server Error', $message));
-  }
+class ErrorBody
+{
+    public function getInternalServerErrorBody($message)
+    {
+        return json_encode($this->getError('Internal Server Error', $message));
+    }
 
-  public function getBadRequestBody($message) {
-    return json_encode($this->getError('Bad Request', $message));
-  }
+    public function getBadRequestBody($message)
+    {
+        return json_encode($this->getError('Bad Request', $message));
+    }
 
-  public function getError($error, $message) {
-    return ["error" => $error, "message" => $message];
-  }
+    public function getError($error, $message)
+    {
+        return ['error' => $error, 'message' => $message];
+    }
 }
