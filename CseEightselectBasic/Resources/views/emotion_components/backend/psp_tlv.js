4a46dcc09cdc3247b3ba7d26ec2861ca42280242
Andreas Kleiber <andreas.kleiber@8select.de>
2018-12-27 21:28:13 +0100

    add commented smarty blocks to emotion ExtJS
    this seems to be necessary sometimes
    
    we just do like the docs say:
    https://developers.shopware.com/developers-guide/custom-shopping-world-elements/#advanced:-adding-a-custom-emotion-component-in-extjs

--- CseEightselectBasic/Resources/views/emotion_components/backend/psp_tlv.js
+++ CseEightselectBasic/Resources/views/emotion_components/backend/psp_tlv.js
@@ -1,6 +1,8 @@
+//{block name="emotion_components/backend/psp_tlv"}
 Ext.define('Shopware.apps.Emotion.view.components.PspTlv', {
 
     extend: 'Shopware.apps.Emotion.view.components.Base',
 
     alias: 'widget.emotion-8select-psptlv-element'
 });
+//{/block}
