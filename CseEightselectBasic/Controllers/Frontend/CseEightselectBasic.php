bb799be6fef3fe7908d8639da3bfc2568250546a
Andreas Kleiber <andreas.kleiber@8select.de>
2019-02-18 15:20:41 +0100

    log error if json_encode fails

--- CseEightselectBasic/Controllers/Frontend/CseEightselectBasic.php
+++ CseEightselectBasic/Controllers/Frontend/CseEightselectBasic.php
@@ -128,14 +128,31 @@ class Shopware_Controllers_Frontend_CseEightselectBasic extends Enlight_Controll
             $offset = filter_var($this->Request()->getParam('offset', 0), FILTER_VALIDATE_INT);
             $isDeltaExport = filter_var($this->Request()->getParam('delta', true), FILTER_VALIDATE_BOOLEAN);
 
+            $data = $export->getProducts($limit, $offset, $isDeltaExport);
             $response = json_encode(
                 [
                     'limit' => $limit,
                     'offset' => $offset,
                     'total' => $export->getTotal($isDeltaExport),
-                    'data' => $export->getProducts($limit, $offset, $isDeltaExport),
+                    'data' => $data,
                 ]
             );
+
+            if ($response === false) {
+                $message = sprintf('json error while exporting');
+                $context = [
+                    'error' => [
+                        'json_last_error' => json_last_error(),
+                        'json_last_error_msg' => json_last_error_msg(),
+                    ],
+                    'skus' => $this->extractSkus($data),
+                ];
+                $logger = $this->container->get('cse_eightselect_basic.setup.helpers.logger');
+                $logger->log('export', [[
+                    'message' => $message,
+                    'context' => $context,
+                ]], true);
+            }
 
             return $this->Response()->setBody($response);
         } catch (\Exception $exception) {
@@ -145,6 +162,20 @@ class Shopware_Controllers_Frontend_CseEightselectBasic extends Enlight_Controll
 
             return;
         }
+    }
+
+    /**
+     * @param array $products
+     * @return array
+     */
+    private function extractSkus($products)
+    {
+        $skus = [];
+        foreach ($products as $product) {
+            $skus[] = $product['sku'];
+        }
+
+        return $skus;
     }
 
     /**
